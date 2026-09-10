a21650ec60e43a0396c6f5d0894a4e266eface70
Nick Miyake <nmiyake@users.noreply.github.com>
2026-09-10 11:05:18 -0700

    Change ordering in 'verify' to run before 'format' (#643)
    Change ordering of "lint" task in "verify" task to run before "format".

--- cmd/plugininfo.go
+++ cmd/plugininfo.go
@@ -37,7 +37,7 @@ var (
 			lintCmd.Short,
 			pluginapi.TaskInfoCommand(lintCmd.Name()),
 			pluginapi.TaskInfoVerifyOptions(
-				pluginapi.VerifyOptionsOrdering(new(verifyorder.Check)),
+				pluginapi.VerifyOptionsOrdering(new(verifyorder.Format-50)),
 				pluginapi.VerifyOptionsApplyTrueArgs("--fix"),
 			),
 		),
